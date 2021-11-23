997bf4277fe82c978a149ba978d3dc116e5cb1b1
Kip <Kipling.Dunlap@gmail.com>
2021-11-22 20:13:09 -0500

    edit seed data format, add assmt

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,29 @@
-INSERT INTO department(dept_name)
+INSERT INTO department
+  (name)
 VALUES  ("Magical Engineering"),
         ("Unicorn Maintenance"),
         ("R&DnD"),
         ("Glitter Removal");
 
-INSERT INTO emp_role (title, salary)
-VALUES  ("O Level Wizard", 10),
-        ("Wizrobe", 15),
-        ("Lead Unicorn Mech", 20),
-        ("Horn Sharpener", 25),
-        ("Lv57 Neckbeard", 30),
-        ("8 Eyed Spiderdweeb", 35),
-        ("NaNaNaNaDustBuster", 40),
-        ("Dr. Ectomy", 45);
+INSERT INTO role
+  (title, salary, department_id)
+VALUES  ("O Level Wizard", 10000, ),
+        ("Wizrobe", 15000, 1),
+        ("Lead Unicorn Mech", 20000, 2),
+        ("Horn Sharpener", 25000, 2),
+        ("Lv57 Neckbeard", 30000, 3),
+        ("8 Eyed Spiderdweeb", 35000, 3),
+        ("NaNaNaNaDustBuster", 40000, 4),
+        ("Dr. Ectomy", 45000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
